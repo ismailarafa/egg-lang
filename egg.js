dc12bc6c3651d2441998ffc75a23564f48743044
Ismail Arafa <ismailarafa18@gmail.com>
2017-07-21 12:19:32 -0400

    implement arrays

--- egg.js
+++ egg.js
@@ -2,6 +2,15 @@ var specialForms = Object.create(null);
 var topEnv = Object.create(null);
 topEnv.true = true;
 topEnv.false = false;
+topEnv.array = function () {
+  return Array.prototype.slice.call(arguments, null);
+};
+topEnv.length = function (array) {
+  return array.length;
+};
+topEnv.element = function (array, n) {
+  return array[n];
+};
 topEnv.print = function (value) {
   console.log(value);
   return value;
@@ -179,8 +188,12 @@ function run() {
   topEnv[op] = new Function('a, b', 'return a ' + op + ' b;');
 });
 
-run("do(define(pow, fun(base, exp,",
-  "     if(==(exp, 0),",
-  "        1,",
-  "        *(base, pow(base, -(exp, 1)))))),",
-  "   print(pow(2, 10)))");
+run("do(define(sum, fun(array,",
+  "     do(define(i, 0),",
+  "        define(sum, 0),",
+  "        while(<(i, length(array)),",
+  "          do(define(sum, +(sum, element(array, i))),",
+  "             define(i, +(i, 1)))),",
+  "        sum))),",
+  "   print(sum(array(1, 2, 3, 4))))");
+// 10
